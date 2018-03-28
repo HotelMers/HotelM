ab0f5088c97584e01528b491083dadb8c10168a0
renlei3 <807588313@qq.com>
2018-03-28 08:28:27 +0800

    创建初始项目
    已经具备登录、注册、登出等功能，并且配置了数据库

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+module.exports = function (app) {
+    app.get('/', function (req, res) {
+      res.redirect('/signin')
+    })
+    app.use('/signin', require('./signin'))
+    app.use('/signout', require('./signout'))
+    app.use('/manage', require('./manage'))
+    app.use('/signup', require('./signup'))
+}

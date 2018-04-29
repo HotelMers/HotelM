af1719631f2c5bb5cecec691c0d0db54fdd97e0e
Huiling So <185236991@qq.com>
2018-04-29 10:45:53 +0800

    管理客房页面增加了所有房间信息表格

--- routes/index.js
+++ routes/index.js
@@ -69,9 +69,9 @@ module.exports = function(app) {
     })
     // add more router
     // 404 page
-    // app.use(function (req, res) {
-    //   if (!res.headersSent) {
-    //     res.status(404).render('404')
-    //   }
-    // })
+    app.use(function (req, res) {
+      if (!res.headersSent) {
+        res.status(404).render('404')
+      }
+    })
 }

71d73b048632d00d80711746b2f8a70bd3af6c68
YUKE LI <yuke@YUKEdeMBP.lan>
2018-05-15 00:29:13 +0800

    真正转成和master一样的版本

--- routes/searchcus.js
+++ routes/searchcus.js
@@ -8,34 +8,37 @@ const checkLogin = require('../middlewares/check').checkLogin
 
 
 module.exports = {
-  //GET 根据填写的信息，查找用户，返回用户信息
-  searchvipPage: function(req, res) {
-    res.render('searchcus');
+  // GET
+  searchcusPage: function (req, res) {
+    res.render('searchcus')
   },
-  //POST 根据填写的信息，查找用户，返回用户信息
-  searchvipSubmit: function(req, res, next) {
-    const idcardnum = req.fields.id.toString()
 
-    //校验参数
+  // POST
+  searchcusSubmit: function(req, res, next) {
+    const id = req.fields.idcard
+  
+    // 校验参数
     try {
-      if (idcardnum.length != 18) {
+      if (id.length != 18) {
         throw new Error('无效身份证号')
       }
     } catch (e) {
-      req.flash('error', '无效身份证号')
-      return res.redirect('searchcus')
+      req.flash('error', e.message)
+      return res.redirect('/searchcus')
     }
-    
-    CusModel.getCusById(idcardnum)
+
+    CusModel.getCusById(id)
       .then(function (customer) {
         if (!customer) {
           req.flash('error', '会员不存在')
-          return res.redirect('searchcus')
+          return res.redirect('/searchcus')
         }
         req.flash('success', '查询成功')
 
-        res.render('searchcus', {customer:customer})
-      })
+        var bookinfo = { id :"", name: "", score : "", phone: ""};
 
-  },
+        res.render('checkin',{customer:customer,bookinfo:bookinfo})
+      })
+      
+  }
 }

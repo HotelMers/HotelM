66e24f0b3083e0baf5ad9da8362ae1c774f40d62
RaoYuxi <805459700@qq.com>
2018-05-15 16:35:07 +0800

    回退

--- routes/balanceclear.js
+++ routes/balanceclear.js
@@ -25,19 +25,7 @@ module.exports = {
     var day = date.getDate();
     var time_in_num= year*10000+month*100+day;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-    for (var i = time_in_num-7; i < time_in_num; i++) {
-=======
     for (var i = time_in_num-7 ; i < time_in_num; i++) {
->>>>>>> master
-=======
-    for (var i = 0; i < ; i++) {
->>>>>>> parent of f10baab... fix bug
-=======
-    for (var i = time_in_num-7 ; i < time_in_num; i++) {
->>>>>>> origin/ryx
       BookModel.getBookinfoByEnddates(i)
         .then(function(result) {
             if (result.enddate< time_in_num) {

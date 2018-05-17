b8d286368e135ae294a4df63571bf4dc07bed50c
Huiling So <185236991@qq.com>
2018-05-17 09:22:31 +0800

    Update manageroom.js

--- routes/manageroom.js
+++ routes/manageroom.js
@@ -12,12 +12,12 @@ module.exports = {
     RoomModel.getAllRoomInfo()
     .then(function (rooms) {
       if (!rooms) {
-        rooms = {number:'0',type:'0',value:'0',status:'0'}
+        rooms = {number:'0',type:'0',value:'0',status:0}
       }
       // 简单处理信息
       for (var i = 0; i < rooms.length; i++) {
         (function (index) {
-          if (rooms[index].status=='0')
+          if (rooms[index].status==0)
             rooms[index].status="无人入住";
         })(i);
       }

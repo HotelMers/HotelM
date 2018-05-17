1d5af0c65a1e2fbb7d91e6e72bd17d55aaa6e0e1
Cassie1213 <31960141+Cassie1213@users.noreply.github.com>
2018-05-17 15:57:44 +0800

    完善生成财务报表功能

--- routes/finance.js
+++ routes/finance.js
@@ -2,13 +2,70 @@ const express = require('express')
 const router = express.Router()
 
 const CusModel = require('../models/customers')
+const RoomModel = require('../models/rooms')
+const CheckInfoModel = require('../models/checkInfo')
 const checkLogin = require('../middlewares/check').checkLogin
 
-router.get('/', checkLogin, function (req, res, next) {
-    res.render('finance')
-})
+//router.get('/', checkLogin, function (req, res, next) {
+//    res.render('finance')
+//})
 
-router.post('/', checkLogin, function (req, res, next) {
-  })
+//router.post('/', checkLogin, function (req, res, next) {
+//  })
 
-module.exports = router
+module.exports = {
+
+	//GET 生成财务报表
+	financePage: function(req, res) {
+		CheckInfoModel.getAllCheckInfo()
+		.then(function(CheckInfo) {
+			return res.render('finance', {CheckInfo:CheckInfo})
+		})
+		//var flag = false
+		//res.render('finance'); //,{flag:flag}
+	},
+
+	//POST 生成财务报表
+	financeSubmit: function(req, res, next) {
+		const startdate = Number(req.fields.starttime)
+		const enddate = Number(req.fields.endtime)
+		let id = req.fields.id
+
+		//校验参数
+		try {
+			if (!startdate || isNaN(startdate)) {
+				throw new Error('请填写开始时间')
+			}
+			if (startdate < 20000000 || startdate > 29999999) {
+				throw new Error('开始时间格式错误，正确格式为20XXXXXX')
+			}
+			if (!enddate || isNaN(enddate)) {
+				throw new Error('请填写结束时间')
+			}
+			if (enddate < 20000000 || enddate > 29999999) {
+				throw new Error('结束时间格式错误，正确格式为20XXXXXX')
+			}
+			if (id !== "forbidden") {
+				throw new Error('管理员码错误')
+			}
+		} catch (e) {
+			req.flash('error', e.message) //e.massage
+			return res.redirect('/finance')
+			//next(e)
+		}
+
+		CheckInfoModel.getCheckInfoByTimeRange(startdate, enddate) //, endtime
+			.then(function(CheckInfo) {
+				if (!CheckInfo) {
+					req.flash('error', '信息不存在')
+					return res.redirect('finance')
+				}
+				//req.flash('success', '成功')
+				//console.log(CheckInfo);
+				//var flag = true
+				//, flag:flag}
+				res.render('finance.ejs', {CheckInfo:CheckInfo})//{RoomNumber:result.RoomNumber, Roomtype:result.Roomtype, starttime:result.startdate, endtime:result.enddate}
+			})
+
+	},
+}
